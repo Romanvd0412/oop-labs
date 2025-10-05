d05ab05e4a2cfac7414331f00b85e92749704f96
Romanvd0412 <64488621+Romanvd0412@users.noreply.github.com>
2025-10-05 23:35:53 +0300

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,11 +6,14 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg {"Привіт,", "це", "моя", "перша", "програма", "на", "C++", "в", "VS Code!"};
+    int count = 0;
 
     for (const string& word : msg)
     {
         cout << word << " ";
+        count++;
     }
     cout << endl;
+    cout << "Кількість слів: " << count << endl;
 }
